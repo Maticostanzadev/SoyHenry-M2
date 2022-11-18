61f329168c85d7696dc9d696ff1771237a25fb9d
Maticostanzadev <maticostanzadev@gmail.com>
2022-11-17 22:38:33 -0300

    Nombre que quieras

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-   return (
-   <div>
+  return (
+    <div>
       <button onClick={props.onClose}>X</button>
-      <h1> { props.name}</h1>
+      <h1> {props.name}</h1>
       <div>
-         <h3>Min {props.min}</h3>
-         <h3>Max {props.max}</h3>
-         <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="img not found"/>
+        <h3>Min: {props.min}</h3>
+        <h3>Max: {props.max}</h3>
+        <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="img not found" />
       </div>
-   </div>
-   );
+    </div>
+  );
 };
